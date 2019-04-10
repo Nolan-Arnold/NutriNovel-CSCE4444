5483f03342d1d921d5107d7e1359de8b2f6d77e1
Stephen <44126889+Stephen10023@users.noreply.github.com>
2019-04-10 14:02:57 -0500

    Add files via upload

---
+++ src/app/plate/plate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plate',
+  templateUrl: './plate.component.html',
+  styleUrls: ['./plate.component.css']
+})
+export class PlateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
