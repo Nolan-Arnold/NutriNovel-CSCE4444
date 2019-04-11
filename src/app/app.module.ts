d9560f41c945034f5340fc8a24c06f925109c63e
AnneMarie <annemarie.sabatini@yahoo.com>
2019-04-11 12:53:19 -0500

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-import { MatSortModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, MatTableModule } from '@angular/material'; //required for using mat related stuff within module
-=======
+
 import { MatSortModule, MatFormFieldModule,
   MatInputModule, MatCheckboxModule, MatTableModule } from '@angular/material'; //required for using mat related stuff within module
->>>>>>> fc767f09a5997cd56b2251e1ed09c611dfe10781
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatMenuModule } from '@angular/material/menu';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-<<<<<<< HEAD
+
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
-=======
-
-import { AppRoutingModule } from './app-routing.module';
->>>>>>> fc767f09a5997cd56b2251e1ed09c611dfe10781
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ListPlaygroundComponent } from './list-playground/list-playground.component';
@@ -48,25 +40,18 @@ import { MessagesComponent } from './messages/messages.component';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-<<<<<<< HEAD
+
     MatMenuModule, //menu
     MatButtonModule, //ripple buttons
     MatPaginatorModule,
     MatToolbarModule,
-    RouterModule
+    RouterModule,
+    HttpClientModule
 
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent] //remove other bootstrap components
-=======
-    MatMenuModule, // menu
-    MatButtonModule, // ripple buttons
-    MatPaginatorModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent] // remove other bootstrap components
->>>>>>> fc767f09a5997cd56b2251e1ed09c611dfe10781
+
 })
 export class AppModule { }
-
--- src/app/list-playground/list-playground.component.ts
+++ src/app/list-playground/list-playground.component.ts
@@ -2,38 +2,7 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort, MatTableDataSource, MatPaginator } from '@angular/material';
 
-<<<<<<< HEAD
-export interface MenuElements {
- restname: string;
- item: string;
- calories: number;
- type: string;
-}
 
-const ELEMENT_DATA: MenuElements[] = [
- {restname: 'McDonald\'s', item: 'Egg McMuffin®', calories: 300, type: 'Breakfast'},
- {restname: 'Whataburger', item: 'Sweet & Spicy Bacon Burger', calories: 1080, type: 'Burger'},
- {restname: 'Whataburger', item: 'Cinnamon Roll', calories: 570, type: 'Dessert'},
- {restname: 'McDonald\'s', item: 'French Fries', calories: 340, type: 'Side'},
- {restname: 'Chick-fil-A', item: 'Waffle Fries', calories: 280, type: 'Side'},
- {restname: 'Burger King', item: 'BK Chicken Fries', calories: 290, type: 'Fries'},
- {restname: 'Five Guys'  , item: 'Burger Bowl', calories: 220, type: 'Burger'},
- {restname: 'Whataburger', item: 'WHATABURGER JR.®', calories: 400, type: 'Burger'},
- {restname: 'Chick-fil-A', item: 'Spicy Southwest Salad', calories: 350, type: 'Salad'},
- {restname: 'McDonald\'s', item: 'Happy Meal®', calories: 501, type: 'Meal'},
- {restname: 'McDonald\'s', item: 'Egg McMuffin®', calories: 300, type: 'Breakfast'},
- {restname: 'Whataburger', item: 'Sweet & Spicy Bacon Burger', calories: 1080, type: 'Burger'},
- {restname: 'Whataburger', item: 'Cinnamon Roll', calories: 570, type: 'Dessert'},
- {restname: 'McDonald\'s', item: 'French Fries', calories: 340, type: 'Side'},
- {restname: 'Chick-fil-A', item: 'Waffle Fries', calories: 280, type: 'Side'},
- {restname: 'Burger King', item: 'BK Chicken Fries', calories: 290, type: 'Fries'},
- {restname: 'Five Guys'  , item: 'Burger Bowl', calories: 220, type: 'Burger'},
- {restname: 'Whataburger', item: 'WHATABURGER JR.®', calories: 400, type: 'Burger'},
- {restname: 'Chick-fil-A', item: 'Spicy Southwest Salad', calories: 350, type: 'Salad'},
- {restname: 'McDonald\'s', item: 'Happy Meal®', calories: 501, type: 'Meal'},
- {restname: 'McDonald\'s', item: 'Happy Deal®', calories: 9001, type: 'Meal'},
-];
-=======
 import { Food } from '../food';
 import { FOODS } from '../mock-foods';
 import { FoodService } from '../food.service';
@@ -42,7 +11,7 @@ import { FoodService } from '../food.service';
 // Removed hardcoded decloration of ELEMENT_DATA array of MenuElements objects
 // Renamed MenuElements to Food, which is now defined in food.ts
 // Renamed ELEMENT_DATA to FOODS, which is an array of food objects
->>>>>>> fc767f09a5997cd56b2251e1ed09c611dfe10781
+
 
 @Component({
  selector: 'app-list-playground',
@@ -51,31 +20,6 @@ import { FoodService } from '../food.service';
 })
 
 export class ListPlaygroundComponent implements OnInit {
-<<<<<<< HEAD
- displayedColumns: string[] = ['select','restname', 'item', 'calories', 'type'];
- dataSource = new MatTableDataSource<MenuElements>(ELEMENT_DATA);
- selection = new SelectionModel<MenuElements>(true, []);
- @ViewChild(MatSort) sort: MatSort;
- @ViewChild(MatPaginator) paginator: MatPaginator;
-
- /** Sorting related */
- ngOnInit() {
-   this.dataSource.sort = this.sort;
-   this.dataSource.paginator = this.paginator;
-  }
-
-   /** Filter related */
- applyFilter(filterValue: string) { //filter related things
-   this.dataSource.filter = filterValue.trim().toLowerCase();
- }
-
- /** Whether the number of selected elements matches the total number of rows. */
- isAllSelected() {
-   const numSelected = this.selection.selected.length;
-   const numRows = this.dataSource.data.length;
-   return numSelected === numRows;
- }
-=======
 
   // Create FOODS, an array of food objects
   // TODO: Currently stores a local array of food objects, must change to using observables
@@ -116,7 +60,6 @@ export class ListPlaygroundComponent implements OnInit {
     const numRows = this.dataSource.data.length;
     return numSelected === numRows;
   }
->>>>>>> fc767f09a5997cd56b2251e1ed09c611dfe10781
 
  /** Selects all rows if they are not all selected; otherwise clear selection. */
  masterToggle() {
@@ -125,4 +68,3 @@ export class ListPlaygroundComponent implements OnInit {
      this.dataSource.data.forEach(row => this.selection.select(row));
    }
 }
-
