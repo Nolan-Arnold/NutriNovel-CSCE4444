009d68b78b69d0d6eba2ce4cf42832ba447f5d41
AnneMarie <annemarie.sabatini@yahoo.com>
2019-04-29 00:53:49 -0500

    plate aesthetics

--- src/app/plate-food.service.ts
+++ src/app/plate-food.service.ts
@@ -9,11 +9,11 @@ export class PlateFoodService {
   comparelist: Food[] = [
       {
           _id: '', restname: 'Fast Food A', item: 'Add 2 Food Items to Compare',
-          calories: 300, carbohydrates: 15, protein: 2, total_fat: 4, type: 'Breakfast'
+          calories: 0, carbohydrates: 0, protein: 0, total_fat: 0, type: 'NA'
       },
       {
           _id: '', restname: 'Fast Food B', item: 'Add 2 Food Items to Compare',
-          calories: 1080, carbohydrates: 13, protein: 8, total_fat: 6, type: 'Burger'
+          calories: 0, carbohydrates: 0, protein: 0, total_fat: 0, type: 'NA'
       }
   ];
   constructor() { }
