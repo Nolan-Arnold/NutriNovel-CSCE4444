34014c8eb38ab094316d0ecb3c4c8ef8a306c3b9
Lupe Rivera <lupe8900@gmail.com>
2019-05-02 13:01:55 -0500

    Start work on admin portal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { CompareComponent } from './compare/compare.component';
-import { HoverMenuComponent } from './hover-menu/hover-menu.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
 import { ExpansionComponent} from './expansion/expansion.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'full-menu', component: SearchComponent },
   { path: 'compare', component: CompareComponent },
-  { path: 'plate', component: ExpansionComponent }
+  { path: 'plate', component: ExpansionComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent }
 ];
 
 @NgModule({
